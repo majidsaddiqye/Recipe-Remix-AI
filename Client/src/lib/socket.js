a58393920db6027dfc3f1c61aacd03be2f6993a3
Abdul Majid <majidsaddiqye@gmail.com>
2025-12-28 03:24:44 +0500

    add env url in axios

--- Client/src/lib/socket.js
+++ Client/src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Backend URL (Apne port ke mutabiq change karein)
-const SOCKET_URL = "http://localhost:3000"; 
+const SOCKET_URL = import.meta.env.VITE_API_URL 
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false, // Dashboard mount hone par manually connect karenge
